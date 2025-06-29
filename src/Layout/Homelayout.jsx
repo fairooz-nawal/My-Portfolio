c2ea454679bccaf91ef9bff5574815fc5d82acc6
Syeda Fairooz Nawal <syedafairooznawal@gmail.com>
2025-06-30 05:08:21 +0600

    Adding the Navbar Section

--- src/Layout/Homelayout.jsx
+++ src/Layout/Homelayout.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navbar from '../Component/Navbar';
+import { Outlet } from 'react-router';
 
 const Homelayout = () => {
     return (
         <div>
-            
+            <Navbar></Navbar>
+            <Outlet></Outlet>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router";
+import Homelayout from './Layout/Homelayout.jsx';
+import Home from './Page/Home.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Homelayout></Homelayout>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      }
+    ]
   },
 ]);
 
